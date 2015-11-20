62104153fca43c33b2a0df20e93ef4e20cefc7ca
Landon Abney <Landonabney@gmail.com>
2015-11-20 13:32:14 -0800

    App specs
    Most of these specs are commented out due to issues with the
    initialization of the tests, to be fixed later.

---
+++ spec/linter-pylint-spec.js
@@ -0,0 +1,73 @@
+'use babel';
+/* eslint-env jasmine */
+/* global waitsForPromise */
+
+describe('The pylint provider for Linter', () => {
+  // const lint = require('../lib/main').provideLinter().lint;
+
+  beforeEach(() => {
+    waitsForPromise(() => {
+      atom.packages.activatePackage('linter-pylint');
+      return atom.packages.activatePackage('language-python').then(() =>
+        atom.workspace.open(__dirname + '/files/good.py')
+      );
+    });
+  });
+
+  it('should be in the packages list', () => {
+    return expect(atom.packages.isPackageLoaded('linter-pylint')).toBe(true);
+  });
+
+  it('should be an active package', () => {
+    return expect(atom.packages.isPackageActive('linter-pylint')).toBe(true);
+  });
+/*
+  describe('checks bad.py and', () => {
+    let editor = null;
+    beforeEach(() => {
+      waitsForPromise(() => {
+        return atom.workspace.open(__dirname + '/files/bad.py').then(openEditor => {
+          editor = openEditor;
+        });
+      });
+    });
+
+    it('finds at least one message', () => {
+      return lint(editor).then(messages => {
+        expect(messages.length).toBeGreaterThan(0);
+      });
+    });
+
+    it('verifies that message', () => {
+      return lint(editor).then(messages => {
+        expect(messages[0].type).toBeDefined();
+        expect(messages[0].type).toEqual('convention');
+        expect(messages[0].html).not.toBeDefined();
+        expect(messages[0].text).toBeDefined();
+        expect(messages[0].text).toEqual('C0111 Missing module docstring');
+        expect(messages[0].filePath).toBeDefined();
+        expect(messages[0].filePath).toMatch(/.+spec[\\\/]files[\\\/]bad\.py$/);
+        expect(messages[0].range).toBeDefined();
+        expect(messages[0].range.length).toEqual(2);
+        expect(messages[0].range).toEqual([[0, 0], [0, 3]]);
+      });
+    });
+  });
+
+  it('finds nothing wrong with an empty file', () => {
+    return atom.workspace.open(__dirname + '/files/empty.py').then(editor => {
+      return lint(editor).then(messages => {
+        expect(messages.length).toEqual(0);
+      });
+    });
+  });
+
+  it('finds nothing wrong with a valid file', () => {
+    return atom.workspace.open(__dirname + '/files/good.py').then(editor => {
+      return lint(editor).then(messages => {
+        expect(messages.length).toEqual(0);
+      });
+    });
+  });
+*/
+});
