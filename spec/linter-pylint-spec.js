e924d105482b41ea81fdf9c9eb077d07a2983a75
Landon Abney <Landonabney@gmail.com>
2017-01-17 16:40:31 -0800

    Fix lint issues

--- spec/linter-pylint-spec.js
+++ spec/linter-pylint-spec.js
@@ -13,18 +13,18 @@ describe('The pylint provider for Linter', () => {
       Promise.all([
         atom.packages.activatePackage('linter-pylint'),
         atom.packages.activatePackage('language-python').then(() =>
-          atom.workspace.open(goodPath)
+          atom.workspace.open(goodPath),
         ),
-      ])
+      ]),
     );
   });
 
   it('should be in the packages list', () =>
-    expect(atom.packages.isPackageLoaded('linter-pylint')).toBe(true)
+    expect(atom.packages.isPackageLoaded('linter-pylint')).toBe(true),
   );
 
   it('should be an active package', () =>
-    expect(atom.packages.isPackageActive('linter-pylint')).toBe(true)
+    expect(atom.packages.isPackageActive('linter-pylint')).toBe(true),
   );
 
   describe('checks bad.py and', () => {
@@ -33,14 +33,14 @@ describe('The pylint provider for Linter', () => {
       waitsForPromise(() =>
         atom.workspace.open(badPath).then((openEditor) => {
           editor = openEditor;
-        })
+        }),
       );
     });
 
     it('finds at least one message', () =>
       waitsForPromise(() =>
-        lint(editor).then(messages => expect(messages.length).toBeGreaterThan(0))
-      )
+        lint(editor).then(messages => expect(messages.length).toBeGreaterThan(0)),
+      ),
     );
 
     it('verifies that message', () =>
@@ -51,24 +51,24 @@ describe('The pylint provider for Linter', () => {
           expect(messages[0].text).toBe('C0111 Missing module docstring');
           expect(messages[0].filePath).toBe(badPath);
           expect(messages[0].range).toEqual([[0, 0], [0, 4]]);
-        })
-      )
+        }),
+      ),
     );
   });
 
   it('finds nothing wrong with an empty file', () => {
     waitsForPromise(() =>
       atom.workspace.open(emptyPath).then(editor =>
-        lint(editor).then(messages => expect(messages.length).toBe(0))
-      )
+        lint(editor).then(messages => expect(messages.length).toBe(0)),
+      ),
     );
   });
 
   it('finds nothing wrong with a valid file', () => {
     waitsForPromise(() =>
       atom.workspace.open(goodPath).then(editor =>
-        lint(editor).then(messages => expect(messages.length).toBe(0))
-      )
+        lint(editor).then(messages => expect(messages.length).toBe(0)),
+      ),
     );
   });
 });
