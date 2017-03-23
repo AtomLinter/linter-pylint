40887d2fef9d2bbe0455adc44eb7f9c7534d792d
Landon Abney <Landonabney@gmail.com>
2017-03-23 16:28:04 -0700

    Update to Linter v2
    Update to Linter v2 and fix several aspects of the package including:
    * Disable lintOnChange
      * Although a handy feature, until pylint allows specifying a filename
        for stdin input, the previous method of copying the current contents
        to a file simply caused more problems than it was worth.
    * Rename `executable` option to `executablePath` to match other linters
      * Includes a backwards compatability check where if the old setting
        had a configured value it is moved to the new one and the old one is
        unset.
    * Code cleanup

--- spec/linter-pylint-spec.js
+++ spec/linter-pylint-spec.js
@@ -46,11 +46,20 @@ describe('The pylint provider for Linter', () => {
     it('verifies that message', () =>
       waitsForPromise(() =>
         lint(editor).then((messages) => {
-          expect(messages[0].type).toBe('convention');
-          expect(messages[0].html).not.toBeDefined();
-          expect(messages[0].text).toBe('C0111 Missing module docstring');
-          expect(messages[0].filePath).toBe(badPath);
-          expect(messages[0].range).toEqual([[0, 0], [0, 4]]);
+          expect(messages[0].severity).toBe('info');
+          expect(messages[0].excerpt).toBe('C0111 Missing module docstring');
+          expect(messages[0].location.file).toBe(badPath);
+          expect(messages[0].location.range).toEqual([[0, 0], [0, 4]]);
+
+          expect(messages[1].severity).toBe('warning');
+          expect(messages[1].excerpt).toBe('W0104 Statement seems to have no effect');
+          expect(messages[1].location.file).toBe(badPath);
+          expect(messages[1].location.range).toEqual([[0, 0], [0, 4]]);
+
+          expect(messages[2].severity).toBe('error');
+          expect(messages[2].excerpt).toBe("E0602 Undefined variable 'asfd'");
+          expect(messages[2].location.file).toBe(badPath);
+          expect(messages[2].location.range).toEqual([[0, 0], [0, 4]]);
         }),
       ),
     );
