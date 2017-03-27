0df0c8dafe3bdffe9a5aca99b5ba5e40e1f8d8aa
Landon Abney <Landonabney@gmail.com>
2017-03-27 11:11:16 -0700

    Add URL to the results
    Provides a link back to http://pylint-messages.wikidot.com/ for each rule.

--- spec/linter-pylint-spec.js
+++ spec/linter-pylint-spec.js
@@ -5,7 +5,10 @@ import * as path from 'path';
 const goodPath = path.join(__dirname, 'files', 'good.py');
 const badPath = path.join(__dirname, 'files', 'bad.py');
 const emptyPath = path.join(__dirname, 'files', 'empty.py');
+
 const lint = require('../lib/main.js').provideLinter().lint;
+
+const wikiURLBase = 'http://pylint-messages.wikidot.com/messages:';
 
 describe('The pylint provider for Linter', () => {
   beforeEach(() => {
@@ -50,16 +53,19 @@ describe('The pylint provider for Linter', () => {
           expect(messages[0].excerpt).toBe('C0111 Missing module docstring');
           expect(messages[0].location.file).toBe(badPath);
           expect(messages[0].location.range).toEqual([[0, 0], [0, 4]]);
+          expect(messages[0].url).toBe(`${wikiURLBase}C0111`);
 
           expect(messages[1].severity).toBe('warning');
           expect(messages[1].excerpt).toBe('W0104 Statement seems to have no effect');
           expect(messages[1].location.file).toBe(badPath);
           expect(messages[1].location.range).toEqual([[0, 0], [0, 4]]);
+          expect(messages[1].url).toBe(`${wikiURLBase}W0104`);
 
           expect(messages[2].severity).toBe('error');
           expect(messages[2].excerpt).toBe("E0602 Undefined variable 'asfd'");
           expect(messages[2].location.file).toBe(badPath);
           expect(messages[2].location.range).toEqual([[0, 0], [0, 4]]);
+          expect(messages[2].url).toBe(`${wikiURLBase}E0602`);
         }),
       ),
     );
